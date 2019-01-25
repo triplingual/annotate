67d99ce8a15864642f21944bc01817a932007548
Niqui O'Neill <doneill>
2019-01-25 17:56:01 -0500

    update dev api

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ echo source ENV/bin/activate
 pip install -r requirements.txt
 python flaskserver.py &
 bundle install
-jekyll serve --force_polling --host='0.0.0.0'
+jekyll serve --force_polling --host='0.0.0.0' --config _config.yml,_config_dev.yml
 pkill -f flask
