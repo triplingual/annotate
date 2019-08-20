669a23e122287004280c647edf8447a40b6397b2
Niqui O'Neill <doneill@ncsu.edu>
2019-08-20 16:28:55 -0400

    make annotate theme

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+bundle install
+bundle exec jekyll serve --force_polling --host='0.0.0.0' --config _config.yml,_config_dev.yml --port='5555' &
 pip install --upgrade --user pip
 pyvenv ENV
 echo source ENV/bin/activate
+cd _site/assets/python
 pip install --user -r requirements.txt
-python flaskserver.py &
-bundle install
-bundle exec jekyll serve --force_polling --host='0.0.0.0' --config _config.yml,_config_dev.yml --port='5555'
-pkill -f flask
+python flaskserver.py
+pkill -f jekyll
--- settings.py
+++
@@ -1,7 +0,0 @@
-filepath = "_annotations"
-github_token = ""
-github_username = ""
-github_repo = ""
-github_url = "https://api.github.com/repos/{}/{}/contents".format(github_username, github_repo)
-github_branch = "master"
-search_filepath = "_annotation_data"
