ac47bdd87ab29999bef07b02d916f56a51f4f20f
Niqui O'Neill <doneill@ncsu.edu>
2019-09-05 16:28:17 -0400

    move custom images folder

---
+++ assets/python/settings.py
@@ -0,0 +1,8 @@
+filepath = "../../../_annotations"
+github_token = ""
+github_username = ""
+github_repo = ""
+github_url = "https://api.github.com/repos/{}/{}/contents".format(github_username, github_repo)
+github_branch = "master"
+search_filepath = "../../../_annotation_data"
+origin_url = "https://dnoneill.github.io/annotate/annotations/"
